9968729d436b2a40502258cd8b950d0aab8df809
florent parcevaux <dev@webups.fr>
2012-11-22 12:56:02 -0500

    début ajout des fichiers de base

---
+++ _README.txt
@@ -0,0 +1,40 @@
+Thème : WP-framework
+
+			** release early, release often **
+
+Ce framework va servir de base la plus simple possible pour la 
+création de site HTML5 propre.
+
+Le fichier function.php sera le plus neutre possible; pour 
+l'améliorer à sa sauce il suffira d'y ajouter des raccourcis.
+
+*****************************************************************
+*																*
+*					COMPOSITION DU FRAMEWORK					*
+*																*
+*****************************************************************
+
+I	- body
+II	- page
+III	- header
+IV	- nav
+V	- section
+VI	- article
+VII	- aside
+VIII- footer
+
+Largeur de base du site = 960px
+L'ajout d'une autre section ne correspond pas au framework mais
+à un nouveau thème.
+
+*****************************************************************
+*																*
+*						MISE À JOUR								*
+*																*
+*****************************************************************
+
+ - v1.0.0 :  version initiale - page HTML5 -
+
+
+
+
---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        
+        <title><?php balise_titre(); ?></title>
+        
+        <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+		<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+		<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
+		<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+		
+		<!-- Google fonts -->
+
+        <!-- Ajout de Favicon -->
+        <link rel="icon" type="image/png" href="images/favicon/favicon-32.png" />
+        
+        <!-- Ajout bouton iphone -->
+        <link rel="apple-touch-icon" href="images/boutoniphone/touch-icon-iphone.png" />
+        
+        <!-- code google verification-->
+        
+        
+	    <!-- code google analytics-->
+	    
+	    
+	    <!-- on appelle le jquery fournis ave WP pour éviter surcharge et conflits -->
+		<?php
+			wp_head();
+			wp_enqueue_script("jquery");
+		 ?>
+	</head>
+
+
+    <body>
+        <div id="page">
+        <!-- Intégralité de la page -->
